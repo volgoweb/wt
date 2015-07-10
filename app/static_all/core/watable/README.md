d7f0f9063bb4b8d5bba30d36315eb8f07988ff87
volgoweb <volgoweb@bk.ru>
2015-07-10 18:15:03 +0300

    исключил ненужные файлы из индекса

--- app/static_all/core/watable/README.md
+++
@@ -1,5 +0,0 @@
-watable
-=======
-
-A jQuery table plugin with sorting, paging, filtering.
-Visit http://wootapa-watable.appspot.com/ for demo and docs.
--- app/static_all/helper/js/init_datepicker.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $('.datepicker-default').not('.datepicker-processed').addClass('datepicker-processed').default_datepicker();
-});
