0d228136b56123ed8a8f6ffbc52de55faf51a04d
volgoweb <volgoweb@bk.ru>
2015-10-14 12:49:02 +0300

    сделал с главной редирект на события

--- app/core/views.py
+++ app/core/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
 
 
 @login_required
 def front_page(request):
-    return render(request, 'core/page.html', {})
+    # return render(request, 'core/page.html', {})
+    return HttpResponseRedirect(reverse_lazy('notification:notifications_list_page'))
