d768eab58103eb9853b92caa00d162a1afe2c026
volgoweb <volgoweb@bk.ru>
2016-01-25 23:09:00 +0300

    исправил опечатку в queryset

--- app/client/models.py
+++ app/client/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class ClientQueryset(models.db.QuerySet):
+class ClientQueryset(models.query.QuerySet):
     def not_deleted(self):
         return self.filter(deleted=False)
 
